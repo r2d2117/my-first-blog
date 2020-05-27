8ef385a163b8655c764c03863c8bed30deb01701
r2d2117 <woodfinemacpherson@gmail.com>
2020-05-27 18:03:02 -0400

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@c7p7w^^vq*aq=%-(x)x+d)99!s38c&rft77&p%&yy2fok4+tx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
